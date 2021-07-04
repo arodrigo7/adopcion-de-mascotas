5a81e95ba61acd5ceb5580c46b293d0eceb9f28b
Alberto <rodrigo_tdf77@hotmail.com>
2021-07-04 20:28:54 -0300

    programa casi terminado

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+function cargar_perro(){
+
 class Mascota {
     constructor( nombre, color_pelo,peso,altura,raza,estadoAdopcion){
         this.nombre=nombre;
@@ -5,7 +7,7 @@ class Mascota {
         this.peso=peso;
         this.altura=altura;
         this.raza=raza;
-        this.estadoAdopcion="En Adopción";
+        this.estadoAdopcion=estadoAdopcion;
     }
     //set modificar el estado de adopcion
     ModificarEstadoAdopcion(estado){
@@ -20,7 +22,40 @@ class Mascota {
 
 mascota1= new Mascota ("chochi","marron",71,60,"pitbull","En Adopción");
 mascota2=new Mascota("pirulo","marron caca",30,20,"caniche","En Adopción");
-console.log(mascota1.estadoAdopcion);
-console.log(mascota1);
-mascota1.estadoAdopcion= "Adoptado";
-console.log(mascota1.estadoAdopcion);
+
+let mascotaNueva
+//array de mascotas
+cantMascotas=[];
+cantMascotas.push(mascota1);
+cantMascotas.push(mascota2);
+let continuar = true
+while(continuar== true){
+    let nombre=prompt("agregar Nombre de Mascota Nueva");
+    let color_pelo=prompt("agregar color de pelo de Mascota Nueva");
+    let peso=prompt("agregar peso de Mascota Nueva");
+    let altura=prompt("agregar altura de Mascota Nueva");
+    let raza=prompt("agregar raza de Mascota Nueva");
+    let estadoAdopcion=prompt("agregar estado de adopción de Mascota Nueva");
+    mascotaNueva=new Mascota(nombre,color_pelo,peso,altura,raza,estadoAdopcion);
+    
+    cantMascotas.push(mascotaNueva);
+    console.log(cantMascotas);
+    continuar = window.confirm("Desea cargar otro Perro");    
+}
+
+//console.log()
+
+//
+//dato=mascota3
+////dato2=mascota4
+//cantMascotas.push(dato);
+//cantMascotas.push(dato2);
+
+}
+
+function cantEnAdopcion(){
+var newArr = cantMascotas.filter(function(el){
+return (el.estadoAdopcion=== "En Adopción")
+})
+console.log(newArr);
+}
